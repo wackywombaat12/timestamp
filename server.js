e6f8daac790f861f2542ebeb5a505a36f64c8378
wackywombaat12 <jackboyle85@gmail.com>
2017-05-17 11:57:35 +0000

    Finished

--- server.js
+++ server.js
@@ -1,8 +1,30 @@
 var express = require("express");
-var app = express()
+var app = express();
+var path = require('path');
+var moment = require('moment');
 
-app.get('/', function (req, res) {
-  res.send('Hello World!')
+app.use(express.static(path.resolve(__dirname, 'client')));
+
+app.get('/:date_string', function (req, res) {
+    
+    var time = moment(req.params.date_string, 'MMMM DD, YYYY', true);
+  
+    if (!time.isValid()) {
+        time = moment.unix(req.params.timestamp);
+    }
+  
+    if (!time.isValid()) {
+        res.json({
+            'unix': null,
+            'humanReadable': null
+        });
+    }
+  
+    res.json({
+        'unix': time.format('X'),
+        'humanReadable': time.format('MMMM DD, YYYY')
+    });
+
 })
 
 app.listen(8080, function () {
